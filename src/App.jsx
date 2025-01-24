b7d066223aa873095e889804541ba27993ed12f1
123456466 <sunji7378@gmail.com>
2025-01-24 15:16:34 +0900

    update : 수정하기 기능

--- src/App.jsx
+++ src/App.jsx
@@ -6,85 +6,113 @@ const App = () => {
 
     const [medals, setMedals] = useState(SAMPLE)
 
-    const [newCountry, setNewContry] = useState(''),
-    [newGold, setNewGold] = useState(''),
-    [newSilver, setNewSilver] = useState(''),
-    [newBronze, setNewBronze] = useState('');
+    const [newCountry, setNewCountry] = useState(''),
+        [newGold, setNewGold] = useState(''),
+        [newSilver, setNewSilver] = useState(''),
+        [newBronze, setNewBronze] = useState('');
 
     const handleSubmit = (element) => {
         element.preventDefault();
 
-        if(!newCountry.trim()){
+        if (!newCountry.trim()) {
             return alert('국가를 입력 해주세요.')
-        }else if(!newGold.trim() || !newSilver.trim() || !newBronze.trim()){
+        } else if (!newGold.trim() || !newSilver.trim() || !newBronze.trim()) {
             return alert('메달 수를 입력 해주세요.')
-        }else if(newGold < 0 || newSilver < 0 || newBronze < 0){
+        } else if (newGold < 0 || newSilver < 0 || newBronze < 0) {
             return alert('숫자가 너무 낮습니다!')
+        } else if (medals.map((List) => (List.country)).includes(newCountry)) {
+            return alert('이미 존재하는 국가입니다.')
         }
 
-        setMedals([...medals, {id: crypto.randomUUID(), country: newCountry, gold: newGold, silver: newSilver, bronze: newBronze}]);
-        setNewContry('');
+        setMedals([...medals, {
+            id: crypto.randomUUID(),
+            country: newCountry,
+            gold: newGold,
+            silver: newSilver,
+            bronze: newBronze
+        }]);
+        setNewCountry('');
         setNewGold('');
         setNewSilver('');
         setNewBronze('');
     }
 
     const countryChange = (element) => {
-        setNewContry(element.target.value);
+        setNewCountry(element.target.value);
     },
-    goldChange = (element) => {
-        setNewGold(element.target.value);
-    },
-    silverChange = (element) => {
-        setNewSilver(element.target.value);
-    },
-    bronzeChange = (element) => {
-        setNewBronze(element.target.value);
-    }
+        goldChange = (element) => {
+            setNewGold(element.target.value);
+        },
+        silverChange = (element) => {
+            setNewSilver(element.target.value);
+        },
+        bronzeChange = (element) => {
+            setNewBronze(element.target.value);
+        }
 
     const amendList = (element) => {
         element.preventDefault()
+
+        if (!medals.find(({ country }) => country === newCountry)) {
+            return alert('국가가 존재하지 않습니다.');
+
+        } else if (!newGold.trim() || !newSilver.trim() || !newBronze.trim()) {
+            return alert('메달 수를 입력 해주세요.');
+
+        } else if (newGold < 0 || newSilver < 0 || newBronze < 0) {
+            return alert('숫자가 너무 낮습니다!');
+
+        } else {
+            const addMedal = medals.map((list) => {
+                return (list.country === newCountry) ? { ...list, gold: newGold, silver: newSilver, bronze: newBronze } : list
+            })
+            setMedals(addMedal)
+            setNewCountry('');
+            setNewGold('');
+            setNewSilver('');
+            setNewBronze('');
+        }
     }
 
     return (
         <main>
             <header>2024 올림픽 메달</header>
             <form className='AddList'>
-                <div className='SetCountry'>
+                <div>
                     <p>국가명</p>
-                    <input type='text' value={newCountry} onChange={countryChange}/>
+                    <input type='text' value={newCountry} onChange={countryChange} />
                 </div>
-                <div className='SetGold'>
+                <div>
                     <p>금메달</p>
-                    <input type='number' value={newGold} onChange={goldChange}/>
+                    <input type='number' value={newGold} onChange={goldChange} />
                 </div>
-                <div className='SetSilver'>
+                <div>
                     <p>은메달</p>
-                    <input type='number' value={newSilver} onChange={silverChange}/>
+                    <input type='number' value={newSilver} onChange={silverChange} />
                 </div>
-                <div className='SetBronze'>
+                <div>
                     <p>동메달</p>
-                    <input type='number' value={newBronze} onChange={bronzeChange}/>
+                    <input type='number' value={newBronze} onChange={bronzeChange} />
                 </div>
-                <button className='CreateListButton'onClick={handleSubmit}>추가하기</button>
-                <button className='AmendListButton' onClick={amendList}>수정하기</button>
+                <button className='ListButton' onClick={handleSubmit}>추가하기</button>
+                <button className='ListButton' onClick={amendList}>수정하기</button>
             </form>
-            
+
             <ul className='ShowList'>
                 <div className='TopList'>
-                    <p>국가명</p>
-                    <p>금메달</p>
-                    <p>은메달</p>
-                    <p>동메달</p>
-                    <p>　</p>
+                    <p className='ListContents'>국가명</p>
+                    <p className='ListContents'>금메달</p>
+                    <p className='ListContents'>은메달</p>
+                    <p className='ListContents'>동메달</p>
+                    <p className='ListContents'>　</p>
                 </div>
                 {medals.map((medalList) => (
                     <div key={medalList.id} className='List'>
-                        <p>{medalList.country}</p>
-                        <p>{medalList.gold}</p>
-                        <p>{medalList.silver}</p>
-                        <p>{medalList.bronze}</p>
-                        <p>　</p>
+                        <p className='ListContents'>{medalList.country}</p>
+                        <p className='ListContents'>{medalList.gold}</p>
+                        <p className='ListContents'>{medalList.silver}</p>
+                        <p className='ListContents'>{medalList.bronze}</p>
+                        <button className='Delete'>Delete</button>
                     </div>
                 ))}
             </ul>
